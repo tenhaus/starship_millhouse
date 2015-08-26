7010a27ceb3c9e80ecccafaa04e4a9345f6d8a15
Chris Hayen <chayen@gmail.com>
2015-08-26 14:03:56 -0400

    Told babel to allow ES7 decorators

--- webpack/config.js
+++ webpack/config.js
@@ -16,7 +16,7 @@ module.exports = {
     module: {
       loaders: [
         {test: /\.css$/, loader: "style!css" },
-        {test: /\.js$/, exclude: /node_modules/, loaders: ["babel-loader"]}
+        {test: /\.js$/, exclude: /node_modules/, loader: "babel-loader", query: {stage: 1}}
       ]
     },
     devtool: '#source-map',
