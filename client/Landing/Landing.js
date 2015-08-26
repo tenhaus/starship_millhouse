4147d660d2b64ef55765c6ebb8c7879d8725c646
Chris Hayen <chayen@gmail.com>
2015-08-26 14:04:12 -0400

    Added landing page to test decorating with @Radium

---
+++ client/Landing/Landing.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Radium from 'radium';
+
+@Radium
+class Landing extends React.Component {
+  render() {
+    return (
+      <div>Landing</div>
+    );
+  }
+}
+
+export default Landing;
--- client/app.js
+++ client/app.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import Landing from './Landing/Landing';
 
 var AppElement = document.getElementById('app');
 
+
 window.onload = function() {
   React.render(
-    <div>Hey, what up, React</div>,
+    <div>
+      Hey, what up, React
+      <Landing />
+    </div>,
     AppElement
   );
 };
