a94ec92d5bc5c9a188191a24019039173443c4df
Chris Hayen <chayen@gmail.com>
2015-08-26 16:58:03 -0400

    material ui up and running

--- client/App/App.js
+++ client/App/App.js
@@ -1,19 +1,30 @@
 import React from 'react';
 
 import Landing from '../Landing/Landing';
-import Mui from 'material-ui';
+import {RaisedButton, Styles} from 'material-ui'
+
+let ThemeManager = new Styles.ThemeManager();
 
 import './_App.scss';
 
 class App extends React.Component {
 
+  getChildContext() {
+    return {
+      muiTheme: ThemeManager.getCurrentTheme()
+    };
+  }
+
   render() {
     return (
       <div>
+        <RaisedButton label='Test' />
         <Landing />
       </div>
     );
   }
 }
+
+App.childContextTypes = {muiTheme: React.PropTypes.object};
 
 export default App;
