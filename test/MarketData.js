e3c65ce6aa6c5db0dbd002ea08eb8ef740ae964d
Chris Hayen <chayen@gmail.com>
2015-08-27 22:55:32 -0400

    Started writing functions for formatting dates and loading symbol data

--- test/MarketData.js
+++ test/MarketData.js
@@ -5,23 +5,32 @@ var _ = require('lodash');
 var MarketData = require('../lib/MarketData');
 
 describe('Symbols', function() {
-
+  
+  // 
   it('should return valid symbols', function(done) {
     _.forEach(MarketData.symbols, function(symbol) {
       if(symbol.Symbol === 'AAPL') done();
     });
   });
-
+  
 });
 
 describe('Sync', function() {
-
+  
+  //
+  if('Should load 20 years if date range not specified', function(done) {
+    
+  });
+  
+  //
   it('Should detect missing symbol data', function(done) {
   });
 
+  //
   it('Should detect partial date range', function(done) {
   });
 
+  //
   it('Should load missing data', function(done) {
   });
 
